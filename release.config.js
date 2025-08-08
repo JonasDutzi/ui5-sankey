713bf6b6707954cba7ebce2f4544c79a28bcc0d5
d070292 <jonas.dutzi@sap.com>
2025-08-08 13:37:05 +0200

    run formatter

--- release.config.js
+++ release.config.js
@@ -1,15 +1,15 @@
 export default {
-  branches: [
-    "main",
-    { name: "next", prerelease: true },
-    { name: "next-major", prerelease: true },
-    { name: "beta", prerelease: true },
-    { name: "alpha", prerelease: true },
-  ],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    "@semantic-release/github",
-  ],
+	branches: [
+		"main",
+		{ name: "next", prerelease: true },
+		{ name: "next-major", prerelease: true },
+		{ name: "beta", prerelease: true },
+		{ name: "alpha", prerelease: true },
+	],
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/npm",
+		"@semantic-release/github",
+	],
 };
