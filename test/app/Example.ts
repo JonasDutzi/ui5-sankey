85bcd6031bd75165bd235c9d249be2551d958af4
d070292 <jonas.dutzi@sap.com>
2025-08-08 14:49:30 +0200

    trigger pipeline

--- test/app/Example.ts
+++ test/app/Example.ts
@@ -4,7 +4,6 @@ import Sankey from "com/sankey/Sankey";
 import { sankeyData } from "./data";
 import ColumnContent from "com/sankey/ColumnContent";
 import Anchor from "com/sankey/Anchor";
-import Item from "com/sankey/Item";
 import Fragment from "sap/ui/core/Fragment";
 import Element from "sap/ui/core/Element";
 import SankeyChart from "com/sankey/SankeyChart";
